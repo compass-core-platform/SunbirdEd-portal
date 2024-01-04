6e1ea3bbc89058e6879c14aa06f4aea8246280b3
DeviGopinath <devigopinath111@gmail.com>
2024-01-04 10:45:15 +0530

    deleteContent call reverted in published

--- src/app/client/src/app/modules/workspace/components/published/published.component.ts
+++ src/app/client/src/app/modules/workspace/components/published/published.component.ts
@@ -359,7 +359,6 @@ export class PublishedComponent extends WorkSpace implements OnInit, AfterViewIn
           element[0].className = 'sb-modal';
       }, 10);
       this.showCollectionLoader = false;
-      // this.deleteContent(param.data.metaData.identifier, param.data.metaData.primaryCategory);
     } else {
       this.workSpaceService.navigateToContent(param.data.metaData, this.state);
     }
@@ -388,7 +387,6 @@ export class PublishedComponent extends WorkSpace implements OnInit, AfterViewIn
           this.deleteContent(this.currentContentId, this.currentPrimaryCategory);
           return;
         }
-        this.deleteContent(this.currentContentId, this.currentPrimaryCategory);
         this.showCollectionLoader = true;
         const collections = _.get(response, 'result.content', []);
 
