3670f69e66d4633a7dabe48fa3ee12010b69d023
DeviGopinath <devigopinath111@gmail.com>
2024-03-04 14:29:46 +0530

    translation strings for network hub added

--- src/app/client/src/app/modules/network-hub/network-hub.module.ts
+++ src/app/client/src/app/modules/network-hub/network-hub.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { NetworkHubRoutingModule } from './network-hub-routing.module';
 import { NetworkService, SbNetworkHubModule, SbNetworkHubRoutingModule } from 'sb-network-hub'
 import { TaxonomyService } from '../../service/taxonomy.service';
+import { ResourceService } from '@sunbird/shared';
 
 const config = {
   domain:'',
@@ -12,7 +13,16 @@ const config = {
   userId:'',
   userName:'',
   userDepartment:'',
-  authorization: ''
+  authorization: '',
+  translation: {
+    noConnectionReq:'',
+    yourConnectionReq: '',
+    peopleYouKnow: '',
+    connection: '',
+    lastAdded: '',
+    sortByName: '',
+    recommended: ''
+  }
  };
 
 @NgModule({
@@ -32,12 +42,19 @@ const config = {
 })
 export class NetworkHubModule {
   userProfile = JSON.parse( localStorage.getItem('userProfile'))
-  constructor(private taxonomyService: TaxonomyService ) {
+  constructor(private taxonomyService: TaxonomyService, private resourceService: ResourceService ) {
     config.userId = this.userProfile.id;
     config.userName = this.userProfile.userName;
     config.userDepartment = this.userProfile.userDepartment;
     this.taxonomyService.getPortalToken().subscribe((res) => {
       config.authorization = res; 
     });
+    config.translation.noConnectionReq = this.resourceService.frmelmnts.networkHub.noConnectionReq;
+    config.translation.yourConnectionReq = this.resourceService.frmelmnts.networkHub.yourConnectionReq;
+    config.translation.peopleYouKnow = this.resourceService.frmelmnts.networkHub.peopleYouKnow;
+    config.translation.connection = this.resourceService.frmelmnts.networkHub.connection;
+    config.translation.lastAdded = this.resourceService.frmelmnts.networkHub.lastAdded;
+    config.translation.sortByName = this.resourceService.frmelmnts.networkHub.sortByName;
+    config.translation.recommended = this.resourceService.frmelmnts.networkHub.recommended;
   }
 }
